e7ed90b8b177ef97d13d7c7e7fd6507553944398
aysxploit <ayesha.imran.a346@outlook.com>
2025-09-21 00:45:21 +0500

    Create Start Script

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-sudo docker run -p 8000:8000 -p 7860:7860 -p 80:80 translation-tool
+# ======================================================================
+# Usage         : ./setup_vnc.sh
+# Notes         : Run as root or with sudo privileges.
+# ======================================================================
+
+    sudo docker run -p 8000:8000 -p 7860:7860 -p 80:80 translation-tool
